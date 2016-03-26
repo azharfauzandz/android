9da239e3a2c79f2af5f61a1ea4c1f85f3e33d0a5
Bardan Putra Prananto <bppdanto@gmail.com>
2016-03-26 18:10:37 +0700

    Menambah list provinsi pada register (UI masih belum benar)

--- app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
+++ app/build/generated/source/r/debug/android/support/v7/recyclerview/R.java
@@ -17,7 +17,7 @@ public final class R {
 		public static final int item_touch_helper_max_drag_scroll_per_frame = 0x7f070064;
 	}
 	public static final class id {
-		public static final int item_touch_helper_previous_elevation = 0x7f0b0005;
+		public static final int item_touch_helper_previous_elevation = 0x7f0c0005;
 	}
 	public static final class styleable {
 		public static final int[] RecyclerView = { 0x010100c4, 0x7f010060, 0x7f010061, 0x7f010062, 0x7f010063 };
